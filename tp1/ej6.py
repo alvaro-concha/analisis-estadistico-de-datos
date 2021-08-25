5b78721a8727ad39494ed635dd3c79727d59ba66
Alvi <alvarogymnas@gmail.com>
2021-08-25 17:12:22 -0300

    added exercises 5-7

---
+++ tp1/ej6.py
@@ -0,0 +1,66 @@
+"""Ejercicio 6: Ayuda a Fermat
+
+La solución de Fermat al problema de la división justa de la bolsa es impráctica cuando quedan muchas rondas por jugar ya que implica contar todas las partidas que dan por ganador a uno de los dos jugadores.
+
+Afortunadamente con simulaciones Monte Carlo es posible encontrar una solución aproximada.
+
+Supongamos que un juego de 51 rondas es interrumpido en la ronda 25 y que Fermat ganó 15 rondas y Pascal 10.
+
+Simular el resto de las rondas para decidir cuál jugador gana la partida.
+
+Repetir este procedimiento 1000 veces para encontrar la división justa de la bolsa.
+"""
+import numpy as np
+import pandas as pd
+from math import comb
+
+np.random.seed(42)
+
+
+class DivisionJusta:
+    """ "Simulacion del problema de division justa de la bolsa en una partida interrumpida."""
+
+    def __init__(self, n_rondas=51, n_inter=25, F_win_inter=15, P_win_inter=10):
+        self.n_rondas = n_rondas
+        self.n_inter = n_inter
+        self.F_win = F_win_inter
+        self.P_win = P_win_inter
+
+    def __call__(self):
+        coin_toss = np.random.choice([0, 1], size=self.n_rondas - self.n_inter)
+        self.F_win += np.sum(coin_toss == 1)
+        self.P_win += np.sum(coin_toss == 0)
+        return {
+            "F_win": self.F_win,
+            "P_win": self.P_win,
+            "Division": self.F_win > self.P_win,
+        }
+
+
+def resultado_exacto(n_rondas=51, n_inter=25, F_win_inter=15, P_win_inter=10):
+    """Division justa exacta, surge de contar todas las realizaciones posibles usando el triangulo de Pascal."""
+
+    n_rest = n_rondas - n_inter
+    r = n_rondas // 2 + 1 - F_win_inter
+    s = n_rondas // 2 + 1 - P_win_inter
+    division = 0
+    for k in range(s):
+        division += comb(r + s - 1, k)
+    division /= 2 ** n_rest
+    return division
+
+
+def ej6():
+    """Ejercicio 6."""
+    n_ensemble = 1000
+    resultados = []
+    for n in range(n_ensemble):
+        juego = DivisionJusta()
+        resultados.append(juego())
+    resultados = pd.DataFrame(resultados)
+    print("Variables Promedios\n", resultados.mean(axis=0))
+    print("Resultado exacto\n", f"Division {resultado_exacto():.3f}")
+
+
+if __name__ == "__main__":
+    ej6()
---
+++ tp1/ej7.py
@@ -0,0 +1,63 @@
+"""Ejercicio 7: El número de la suerte
+
+Simular el lanzamiento de cinco dados y contar cuántas veces sale el número tres.
+
+Repetir la operación 1000 veces y construir un histograma de frecuencia con el número de ocurrencias del número tres.
+
+Comparar los datos simulados con una función de distribución binomial con parámetros $n$ y $p$ adecuados.
+"""
+import numpy as np
+import pandas as pd
+from scipy.stats import binom
+import plotly.graph_objects as go
+
+np.random.seed(42)
+
+
+class LanzarDados:
+    """ "Simulacion de un lanzamiento de dados."""
+
+    def __init__(self, n_dados=5, n_target=3):
+        self.n_dados = n_dados
+        self.n_target = n_target
+
+    def __call__(self):
+        dice_toss = np.random.choice(range(1, 7), size=self.n_dados)
+        return {
+            "k": (dice_toss == self.n_target).sum(),
+        }
+
+
+def distribucion_exacta(n=5, p=1 / 6):
+    """Distribucion exacta (binomial), del problema de lanzamiento de dados."""
+
+    k = np.arange(n + 1)
+    return k, binom(n=n, p=p).pmf(k)
+
+
+def ej7():
+    """Ejercicio 7."""
+    n_ensemble = 1000
+    resultados = []
+    for n in range(n_ensemble):
+        juego = LanzarDados()
+        resultados.append(juego())
+    resultados = pd.DataFrame(resultados)
+    k, dist = distribucion_exacta()
+    fig = go.Figure()
+    fig.add_trace(
+        go.Histogram(x=resultados["k"], histnorm="probability", name="Simulación")
+    )
+    fig.add_trace(go.Scatter(x=k, y=dist, name="Binomial"))
+    fig.update_traces(opacity=0.75)
+    fig.update_layout(
+        title_text="El número de la suerte",
+        xaxis_title_text="Ocurrencias",
+        yaxis_title_text="Probabilidad",
+        bargap=0.2,
+    )
+    fig.show()
+
+
+if __name__ == "__main__":
+    ej7()
